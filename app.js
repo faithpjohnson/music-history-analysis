ee687ed8851ed004df84ad6443d422bd4a18f56a
Faith Johnson <faithpjohnson@gmail.com>
2022-05-13 16:25:21 -0600

    add functionality to find top 10 artists

--- app.js
+++ app.js
@@ -6,58 +6,88 @@ fetch("./listen_history.json")
   })
   .then((jsonData) => {
     // appendData(jsonData); // appendData will be a function to render data
-    sortCounts(jsonData)
+    sortTitleCounts(jsonData);
+    sortArtistCounts(jsonData);
     console.log(jsonData); // print data to the console
   });
 
-// // Function to render data
-// function appendData(jsonData) {
-//   // loop through data and find titles
-//   for (let i = 0; i < jsonData.length; i++) {
-//     let title = jsonData[i].title;
-//     console.log(title);
-//     // append data
-//     const mainContainer = document.getElementById("data");
-//     const li = document.createElement("li");
-//     li.innerHTML = `TITLE: ${title}`;
-//     mainContainer.append(li);
-//   }
-// }
+function getArtistFromOccurrence(occurrence) {
+  let artist = "Unknown";
+  if (occurrence.subtitles) {
+    artist = occurrence.subtitles[0].name;
+  }
+  return artist;
+}
 
 // Function to count occurances
-function countOccurrences(data) {
-    let counts = {};
-    for (let i = 0; i < data.length; i++) {
-        let title = data[i].title;
-        if (counts[title]) {
-            counts[title] += 1;
-        } else {
-            counts[title] = 1;
-        }
+function countTitleOccurrences(data) {
+  let titleCounts = {};
+
+  for (let i = 0; i < data.length; i++) {
+    let title = data[i].title;
+
+    if (titleCounts[title]) {
+      titleCounts[title] += 1;
+    } else {
+      titleCounts[title] = 1;
     }
-    return counts;
+  }
+  console.log(titleCounts);
+  return titleCounts;
+}
+
+function countArtistOccurrences(data) {
+  let artistCounts = {};
+
+  for (let i = 0; i < data.length; i++) {
+    let artist = getArtistFromOccurrence(data[i]);
+
+    if (artistCounts[artist]) {
+      artistCounts[artist] += 1;
+    } else {
+      artistCounts[artist] = 1;
+    }
+  }
+  console.log(artistCounts);
+  return artistCounts;
 }
 
 // Function to sort the occurances
-function sortCounts(jsonData) {
-    let counts = countOccurrences(jsonData);
-    // sort counts
-    let sortedCounts = Object.keys(counts).sort(function (a, b) {
-        return counts[b] - counts[a];
-    })
-    // find top 10 using slice
-    let top10 = sortedCounts.slice(0, 10);
-    console.log(top10)
-    // render top 10
-    for (let i = 0; i < top10.length; i++) {
-        const title = top10[i].replace('Watched', 'TITLE:')
-        const mainContainer = document.getElementById("data");
-        const li = document.createElement("li");
-        li.innerHTML = `${title}`;
-        mainContainer.append(li);
-    }
+function sortTitleCounts(jsonData) {
+  let counts = countTitleOccurrences(jsonData);
+  // sort counts
+  let sortedCounts = Object.keys(counts).sort(function (a, b) {
+    return counts[b] - counts[a];
+  });
+  // find top 10 using slice
+  let top10 = sortedCounts.slice(0, 10);
+  console.log(top10);
+  // render top 10
+  for (let i = 0; i < top10.length; i++) {
+    const title = top10[i].replace("Watched", "TITLE:");
+    const mainContainer = document.getElementById("data");
+    const li = document.createElement("li");
+    li.innerHTML = `${title}`;
+    mainContainer.append(li);
+  }
 }
 
-
-
-// Render the top 10 to the DOM
+// Function to sort the occurances
+function sortArtistCounts(jsonData) {
+  let counts = countArtistOccurrences(jsonData);
+  // sort counts
+  let sortedCounts = Object.keys(counts).sort(function (a, b) {
+    return counts[b] - counts[a];
+  });
+  // find top 10 using slice
+  let top10 = sortedCounts.slice(0, 10);
+  console.log(top10);
+  // render top 10
+  for (let i = 0; i < top10.length; i++) {
+    const artist = top10[i].replace("- Topic", "");
+    const mainContainer = document.getElementById("artist-data");
+    const li = document.createElement("li");
+    li.innerHTML = `ARTIST: ${artist}`;
+    mainContainer.append(li);
+  }
+}
